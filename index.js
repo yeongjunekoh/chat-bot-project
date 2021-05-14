ccbb15d311bb133ea3f6b90fda7fd78720344fce
yeongjunekoh <rhdudwns0@naver.com>
2021-05-14 11:10:50 +0900

    [FEATURE] Generate text Query routes

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ const config = require("./server/config/keys");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-app.use('/api/dialogflow', require('./server/routes/dialogflow'));
+app.use("/api/dialogflow", require("./server/routes/dialogflow"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-
   // Set static folder
   app.use(express.static("client/build"));
 
@@ -30,5 +28,5 @@ if (process.env.NODE_ENV === "production") {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server Running at ${port}`)
+  console.log(`Server Running at ${port}`);
 });
--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -1,83 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const structjson = require('./structjson.js');
-const dialogflow = require('dialogflow');
-const uuid = require('uuid');
+const structjson = require("./structjson.js");
+const dialogflow = require("dialogflow");
+const uuid = require("uuid");
 
-const config = require('../config/keys');
+const config = require("../config/keys");
+//We need projectId, sessionId to make new session
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+const languageCode = config.dialogFlowSessionLanguageCode;
 
-const projectId = config.googleProjectID
-const sessionId = config.dialogFlowSessionID
-const languageCode = config.dialogFlowSessionLanguageCode
-
-
-// Create a new session
+// Create a new session(When we send request the dialog API We need to create new session)
 const sessionClient = new dialogflow.SessionsClient();
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-// We will make two routes 
+//We will make two routes
 
+//Text Query routes
 
-// Text Query Route
+router.post("/textQuery", async (req, res) => {
+  //We need to send some information that comes from client to the Dialogflow API
+  //How to send information?
 
-router.post('/textQuery', async (req, res) => {
-    //We need to send some information that comes from the client to Dialogflow API 
-    // The text query request.
-    const request = {
-        session: sessionPath,
-        queryInput: {
-            text: {
-                // The query to send to the dialogflow agent
-                text: req.body.text,
-                // The language used by the client (en-US)
-                languageCode: languageCode,
-            },
-        },
-    };
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: req.body.text,
+        //This Text not a hard coding but dynamic coding
+        // The language used by the client (en-US)
+        languageCode: languageCode,
+      },
+    },
+  };
 
-    // Send request and log result
-    const responses = await sessionClient.detectIntent(request);
-    console.log('Detected intent');
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
 
-    res.send(result)
-})
+  //send back responses to front
+  res.send(result);
+});
 
-
-
-//Event Query Route
-
-router.post('/eventQuery', async (req, res) => {
-    //We need to send some information that comes from the client to Dialogflow API 
-    // The text query request.
-    const request = {
-        session: sessionPath,
-        queryInput: {
-            event: {
-                // The query to send to the dialogflow agent
-                name: req.body.event,
-                // The language used by the client (en-US)
-                languageCode: languageCode,
-            },
-        },
-    };
-
-    // Send request and log result
-    const responses = await sessionClient.detectIntent(request);
-    console.log('Detected intent');
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-
-    res.send(result)
-})
-
-
-
-
-
-
+//Event Query routes
 
 module.exports = router;
