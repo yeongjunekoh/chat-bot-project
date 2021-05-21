208e3c29147e33588ab963e0936a2f916da11626
yeongjunekoh <rhdudwns0@naver.com>
2021-05-21 11:54:04 +0900

    [FEATURE] Generate event Query

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -50,4 +50,34 @@ router.post("/textQuery", async (req, res) => {
 
 //Event Query routes
 
+router.post("/eventQuery", async (req, res) => {
+  //We need to send some information that comes from client to the Dialogflow API
+  //How to send information?
+
+  console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      event: {
+        // The query to send to the dialogflow agent
+        name: req.body.event,
+        //This Text not a hard coding but dynamic coding
+        // The language used by the client (en-US)
+        languageCode: languageCode,
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+
+  //send back responses to front
+  res.send(result);
+});
+
 module.exports = router;
