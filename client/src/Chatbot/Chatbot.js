84bd984dd99c2c8fa375349052dd99b7d115e039
yeongjunekoh <rhdudwns0@naver.com>
2021-05-25 11:22:09 +0900

    [FEATURE] Generate textQuery function
    - Error: need to add dispatch

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -1,6 +1,48 @@
 import React from "react";
+import { text } from "body-parser";
+import Axios from "axios";
 
 function Chatbot() {
+  const textQuery = async () => {
+    //first need to take care of message I sent
+    let conversation = {
+      who: "user",
+      content: {
+        text: {
+          text: text,
+        },
+      },
+    };
+
+    const textQueryVariables = { text };
+
+    //we need to take care of the message the chatbot sent
+    try {
+      //I will send request to the textQuery Route
+      const response = await Axios.post(
+        "/api/dialogflow/textQuery",
+        textQueryVariables
+      );
+      const content = response.data.fulfillmentMessages[0];
+
+      conversation = {
+        who: "bot",
+        content: content,
+      };
+      console.log(conversation);
+    } catch (error) {
+      conversation = {
+        who: "bot",
+        content: {
+          text: {
+            text: "Error just ocurred, please check the problem",
+          },
+        },
+      };
+      console.log(conversation);
+    }
+  };
+
   const keyPressHandler = (e) => {
     //we type Enter. not enter
     if (e.key === "Enter") {
@@ -8,7 +50,7 @@ function Chatbot() {
         alert("you need to type something first");
       }
       //we will send request to text query route -> we have to make textQuery function
-      //textQuery(e.target.value);
+      textQuery(e.target.value);
 
       e.target.value = "";
     }
